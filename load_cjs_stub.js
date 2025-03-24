92ba8cbd82a8ed62d15d99be327fefb8a91d0b86
chirsz-ever <chirsz-ever@outlook.com>
2025-03-24 15:28:41 +0800

    update load_cjs_stub

--- load_cjs_stub.js
+++ load_cjs_stub.js
@@ -1,17 +1,18 @@
+var load_cjs_stub;
 /**
- * @param {string} path
- * @param {string} content
+ * You can load this file as script in browser, or read the content and eval it.
+ * @param {string} path must be absolute path
+ * @param {string} content must be string
  * @returns {*}
  */
-function load_cjs_stub(path, content) {
-    const require = () => { throw new Error("Not implemented require in load_cjs_stub") };
-    const module = { exports: {} };
-    const __filename = path;
-    const __dirname = path.includes("/") ? path.replace(/\/[^\/]+\/*$/, "") : ".";
-    const args = [module.exports, require, module, __filename, __dirname];
-    Function('exports', 'require', 'module', '__filename', '__dirname', content).apply(module.exports, args);
+load_cjs_stub = function load_cjs_stub(path, content, fn, err) {
+    var fn_ = typeof fn == 'undefined' ? Function : fn;
+    var err_ = typeof err == 'undefined' ? Error : err;
+    var require = () => { throw new err_("Not implemented require in load_cjs_stub") };
+    var module = { exports: {} };
+    var __filename = path;
+    var __dirname = path.indexOf("/") < 0 ? "." : path.replace(/\/[^\/]+\/*$/, "");
+    var args = [module.exports, require, module, __filename, __dirname];
+    fn_('exports', 'require', 'module', '__filename', '__dirname', content).apply(module.exports, args);
     return module.exports;
-}
-
-// for eval
-load_cjs_stub;
+};
